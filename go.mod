18c6a7eef13f9b382b9c25446dc1b7e79b9790f4
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-11-10 12:47:26 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module winden-feedback
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+// Listens on an HTTP port for POST method.
+// if the post JSON payload is valid, it is emailed to a fixed address.
+// Address to email to is configurable.
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handlePost(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling a post request to feedback url")
+	fmt.Fprintf(w, "post\n")
+}
+
+func main() {
+	toAddressPtr := flag.String("to", "feedback@winden.app", "email address to which feedback is to be sent")
+	flag.Parse()
+
+	// XXX: parse the email address to make sure it is a valid one.
+	log.Printf("feedback email would be send to the address: %s\n", *toAddressPtr)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/feedback", handlePost).Methods("POST")
+
+
+	srv := &http.Server{
+		Addr: ":8001",
+		Handler: r,
+	}
+
+	srv.ListenAndServe()
+}
